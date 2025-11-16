418e92138571b4f5d351cf63dcbe598ca9e10f47
manthansinnarkar <mangitfit@github.com>
2025-11-16 19:53:39 +0530

    git commit -m "commenting SimpleNavigationTest.java"

--- src/test/java/tests/SimpleNavigationTest.java
+++ src/test/java/tests/SimpleNavigationTest.java
@@ -1,25 +1,25 @@
-package tests;
-
-import base.BaseTest;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class SimpleNavigationTest extends BaseTest {
-
-    @Test
-    public void testSimpleNavigation() {
-        System.out.println("Current URL: " + driver.getCurrentUrl());
-        System.out.println("Page Title: " + driver.getTitle());
-        Assert.assertTrue(driver.getTitle().contains("Amazon"), "Should be on Amazon page");
-    }
-
-    // You can add more simple tests here
-    @Test
-    public void testBrowserIsWorking() {
-        String title = driver.getTitle();
-        System.out.println("Browser is working. Page title: " + title);
-        Assert.assertNotNull(title, "Page title should not be null");
-        Assert.assertFalse(title.isEmpty(), "Page title should not be empty");
-
-    }
-}
+//package tests;
+//
+//import base.BaseTest;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//public class SimpleNavigationTest extends BaseTest {
+//
+//    @Test
+//    public void testSimpleNavigation() {
+//        System.out.println("Current URL: " + driver.getCurrentUrl());
+//        System.out.println("Page Title: " + driver.getTitle());
+//        Assert.assertTrue(driver.getTitle().contains("Amazon"), "Should be on Amazon page");
+//    }
+//
+//    // You can add more simple tests here
+//    @Test
+//    public void testBrowserIsWorking() {
+//        String title = driver.getTitle();
+//        System.out.println("Browser is working. Page title: " + title);
+//        Assert.assertNotNull(title, "Page title should not be null");
+//        Assert.assertFalse(title.isEmpty(), "Page title should not be empty");
+//
+//    }
+//}
